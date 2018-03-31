d26541cca96624adc520d446cacb9cc8fd3f82c2
Will Lacey <WillLacey@Will-Laceys-MacBook-Pro.local>
2018-03-30 23:37:10 -0500

    Created Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,59 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(IOLOOP)
+	@KBD // if(key)
+	D=M
+	@BLACK
+	D;JNE // turn screen black
+	@WHITE
+	0;JMP // else turn screen white
+
+(WHITE) // Change the screen color bit to white
+	@color
+	M=0
+	@FILL 
+	0;JMP
+
+(BLACK) // Change the screen color bit to black
+	@color
+	M=-1
+	@FILL 
+	0;JMP
+
+(FILL) // Fill in screen with screen color bit
+	@SCREEN
+	D=A
+	@index
+	M=D
+	@8192
+	D=A
+	@index
+	M=M+D
+
+	(LOOP)
+		@color
+		D=M
+		@index
+		A=M
+		M=D
+		D=A
+
+		@temp // temp = &index - &Screen
+		M=D
+		@SCREEN
+		D=A
+		@temp
+		D=D-M
+
+		@IOLOOP // if(screen filled or if temp == 0) repeat IOLOOP
+		D;JEQ
+		@index
+		M=M-1 // index--
+		@LOOP // else continue filling screen
+		0;JMP
+
+
+
+
