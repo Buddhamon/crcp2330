6efe41bf858aedb6488dd9f10353dab7a313969b
Will Lacey <WillLacey@Will-Laceys-MBP.wifilink.net>
2018-03-30 20:13:05 -0500

    Successfully Created mult.hack

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@R2 // r2 = 0; 
+	M=0
+
+	@R1 // temp = r1;
+	D=M
+	@temp
+	M=D
+
+	@R0 // if(r0 == 0) end program
+	D=M
+	@END
+	D;JEQ
+
+(MULTLOOP) // while(temp != 0)
+	@temp
+	D=M
+	@END
+	D;JEQ
+
+	@R0 // r2 += r0
+	D=M
+	@R2
+	M=D+M
+
+	@temp // temp--;
+	M=M-1
+
+	@MULTLOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
