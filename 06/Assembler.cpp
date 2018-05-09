df2bb4029cb88e401164db12563bd4e88c275568
Buddhamon <wlacey@mail.smu.edu>
2018-05-09 08:30:40 -0500

    removed comments, whitespace, and empty lines

--- 06/Assembler.cpp
+++ 06/Assembler.cpp
@@ -6,19 +6,32 @@
 
 using namespace std;
 
-void readInputFile(char*);
-void outputFile(char*);
+void readASMMFile(char*);
+void removeComments();
+void removeWhiteSpace();
+void deleteEmptyLines();
+void outputHackFile(char*);
 
 vector<string> linesOfCode;
 
 int main(int argc, char* inputFile[])
 {
-	readInputFile(inputFile[1]);
-	outputFile(inputFile[1]);
+	if(argc < 2)
+	{
+		cout << "Error: No File Argument Found" << endl;
+		exit(1);
+	}
+	readASMMFile(inputFile[1]);
+
+	removeComments();
+	removeWhiteSpace();
+	deleteEmptyLines();
+
+	outputHackFile(inputFile[1]);
 	return 0;
 }
 
-void readInputFile(char* inputFile)
+void readASMMFile(char* inputFile)
 {
 	string line = "";
 	ifstream file(inputFile);
@@ -29,15 +42,63 @@ void readInputFile(char* inputFile)
 	}
 }
 
-void outputFile(char* inputFile)
+void removeComments()
+{
+	string comments = "//";
+	for(int i = 0; i < linesOfCode.size()-1; i++)
+	{
+		bool noCommentFound = true;
+		int lineLength = linesOfCode[i].length();
+		for(int j = 0; j < lineLength && noCommentFound; j++)
+		{
+			string potentialComments = linesOfCode[i].substr(j, 2);
+			if(comments.compare(potentialComments) == 0)
+			{
+				noCommentFound = false;
+				int newLineLength = lineLength - (lineLength - j);
+				string shortenedLine = linesOfCode[i].substr(j, newLineLength);
+				linesOfCode[i] = shortenedLine;
+			}
+		}
+	}
+}
+
+void removeWhiteSpace()
+{
+	for(int i = 0; i < linesOfCode.size(); i++)
+	{
+		for(int j = 0; j < linesOfCode[i].length(); j++)
+		{
+			char c = linesOfCode[i].at(j);
+			if(isspace(c))
+			{
+				linesOfCode[i].erase(remove(linesOfCode[i].begin(), linesOfCode[i].end(), c), linesOfCode[i].end());
+			}
+		}
+	}
+}
+
+void deleteEmptyLines()
+{
+	for(int i = linesOfCode.size()-1; i >= 0; i--)
+	{
+		if(linesOfCode[i].length() == 0)
+		{
+			linesOfCode.erase(linesOfCode.begin() + i);
+		}
+	}
+}
+
+void outputHackFile(char* inputFile)
 {
 	string outputFile = inputFile;
 	outputFile = outputFile.substr(0, outputFile.length()-3) + "hack";
 	ofstream file(outputFile);
-	for(int i = 0; i < linesOfCode.size(); i++)
+	for(int i = 0; i < linesOfCode.size()-1; i++)
 	{
 		file << linesOfCode[i] << endl;
 	}
+	file << linesOfCode[linesOfCode.size()-1];
 }
 
 
